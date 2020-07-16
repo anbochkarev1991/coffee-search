2332a42fa66f805bbce62ab63777d4958e85977a
Alex K <smthng@yandex.ru>
2020-07-16 17:02:00 +0300

    full model

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,46 @@
 import mongoose from 'mongoose';
+import { isEmail } from 'validator';
 
 const { model, Schema } = mongoose;
 
 const userSchema = new Schema({
   login: {
+    trim: true,
     type: String,
-    required: true,
+    required: 'Login is required',
+    unique: true,
     minlength: 3,
     maxlength: 15,
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: 'Email address is required',
+    validate: [isEmail, 'Invalid email format'],
+  },
+  password: {
+    type: String,
+    required: 'Password is required',
+    minlength: 6,
+    maxlength: 20,
+  },
+  registered: {
+    type: Date,
+    default: Date.now,
+  },
+  birthday: {
+    type: Date,
+  },
+  favorites: {
+    type: [Schema.Types.ObjectId],
+  },
+  subscriptions: {
+    type: [Schema.Types.ObjectId],
+  },
+  sex: {
+    type: String,
   },
 });
 
