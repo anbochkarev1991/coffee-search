1723079050234ffe97f16351ff3eef339a2296e9
Alex K <smthng@yandex.ru>
2020-07-17 15:25:17 +0300

    Links

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/home/home';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Router>
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/logout">Logout</Link>
+      <Link to="/profile">Profile</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/registration">Registration</Link>
+    </>
+  );
+}
+
+export default Navbar;
