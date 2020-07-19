549647021f1435d18d2704ccc52e815446ef61da
anbochkarev1991 <a.n.bochkarev1991@gmail.com>
2020-07-19 12:18:55 +0300

    Map in progress

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import dotenv from 'dotenv';
+dotenv.config();
 
 function Map() {
+
+  useEffect(() => {
+    window.addEventListener('load', handleLoad());
+  }, [])
+
+  function handleLoad() {
+    window.ymaps.ready(() => {
+      const newMap = new window.ymaps.Map('map', {
+        center: [55.76, 37.64],
+        zoom: 10,
+      })
+    })
+  }
+
   return (
     <div>
       <h3>Beautiful map</h3>
-      <div></div>
+      <div id='map' style={{width: "600px", height: "400px"}}></div>
     </div>
   );
 }
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -14,9 +14,9 @@ function Home() {
 
   return (
     <>
-      {/* <Trends />
+      {/* <Trends /> */}
       <Map />
-      <List /> */}
+      {/* <List /> */}
     </>
   );
 }
