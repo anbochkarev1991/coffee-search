43a7295d763af8c52431f7a2b48d3b8bd79aba3a
anbochkarev1991 <a.n.bochkarev1991@gmail.com>
2020-07-22 10:52:49 +0300

    cafe-search done

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -9,7 +9,9 @@ dotenv.config();
 function Map() {
   const dispatch = useDispatch();
   const [mapActive, setMapActive] = useState(false);
- 
+  const [newMap, setNewMap] = useState();
+  const [error, setError] = useState(false);
+
   const cafes = useSelector((state) => state.coffee.list);
   const user = useSelector((state) => state.enter.login);
   const search = useSelector((state) => state.coffee.search);
@@ -20,34 +22,47 @@ function Map() {
     }
   }, [cafes]);
 
-  // useEffect(()=>{
-  //   console.log('hg');
-  // },[inputs])
-  
   function handleChange({ target: { name, value } }) {
     dispatch(searchCafe({
       ...search,
       [name]: value,
     }))
   }
-  
+
+  async function searchCoordinates(address) {
+    const response = await fetch(`https://geocode-maps.yandex.ru/1.x/?apikey=95ef75db-c7f0-447b-9810-88ce1efe26d6&geocode=${address}&format=json`);
+    let result = await response.json();
+    result = result.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(' ').reverse();
+    return [Number(result[0]), Number(result[1])];
+  }
+
   async function handleSubmit(event) {
-    // event.preventDefault();
-    const cafe = cafes.find((cafe) => cafe.name === search.name);
-    console.log(cafe);
-    if (cafe) {
-      dispatch(searchCafe({
-        ...search,
-        id: cafe._id,
-        rating: cafe.rating,
-        latitude: cafe.latitude,
-        longitude: cafe.longitude,
-      }))
+    event.preventDefault();
+    try {
+      const coords = await searchCoordinates(search.address);
+      console.log(coords);
+      const cafe = cafes.find((cafe) => cafe.name.toLowerCase() === search.name.toLowerCase() && cafe.latitude === coords[0] && cafe.longitude === coords[1]);
+      console.log(cafe);
+      if (coords && cafe) {
+        dispatch(searchCafe({
+          ...search,
+          id: cafe._id,
+          name: cafe.name,
+          // address: cafe.address,
+          rating: cafe.rating,
+          latitude: cafe.latitude,
+          longitude: cafe.longitude,
+        }));
+        newMap.setCenter(coords)
+      }
+    } catch (err) {
+      console.log(err);
+      setError('Неправильно введено название и/или адрес кафе');
     }
   }
 
   function handleLoad() {
-    
+
     navigator.geolocation.getCurrentPosition(function (position) {
       const coords = search.latitude ? [search.latitude, search.longitude] : [position.coords.latitude, position.coords.longitude];
       window.ymaps.ready(() => {
@@ -60,7 +75,7 @@ function Map() {
             present: 'islands#icon',
             iconColor: '#ff6347',
           })
-      
+
         for (let i = 0; i < cafes.length; i++) {
           cafesCollection.add(new window.ymaps.Placemark(search.latitude ? [search.latitude, search.longitude] : [cafes[i].latitude, cafes[i].longitude], {
             balloonContentHeader: `<a href=/cafes/${search.id ? search.id : cafes[i]._id}/menu>${search.name ? search.name : cafes[i].name}</a>`,
@@ -77,6 +92,8 @@ function Map() {
         })
 
         newMap.geoObjects.add(myPlacemark);
+
+        setNewMap(newMap)
       });
     });
   }
@@ -87,17 +104,27 @@ function Map() {
       <div className={styles.mapPosition}>
         <div id="map" style={{ width: "700px", height: "600px" }}></div>
       </div>
-      <form className={"form-inline"} onSubmit={handleSubmit}>
-        <input 
-          className={"form-control mr-sm-2"}
-          style={{ width: "600px", margin: "0 0 0 20px" }}
+      <form className={"form-inline justify-content-center"} onSubmit={handleSubmit}>
+        {error && <h4 className={"form-control mr-sm-2"} style={{ color: "tomato", border: "1px solid lightgrey", width: "600px" }}>{error}</h4>}
+        <input
+          className={"form-control mr-sm-2 d-flex"}
+          style={{ width: "285px", margin: "0 0 0 20px" }}
           type="search"
           name="name"
-          onChange={handleChange} 
-          placeholder="Найти кафе"
-          aria-label="Search" 
+          onChange={handleChange}
+          placeholder="Название"
+          aria-label="Search"
         />
-        <button className={"btn btn-outline-success my-2 my-sm-0"} type="submit" style={{ margin: "0 0 0 20px" }}>Search</button>
+        <input
+          className={"form-control mr-sm-2"}
+          style={{ width: "285px", margin: "0 0 0 20px" }}
+          type="search"
+          name="address"
+          onChange={handleChange}
+          placeholder="Адрес"
+          aria-label="Search"
+        />
+        <button className={"btn btn-outline-success my-2 my-sm-0"} type="submit" style={{ margin: "0 0 0 20px" }}>Искать</button>
       </form>
       {user && <AddCafe />}
     </>
