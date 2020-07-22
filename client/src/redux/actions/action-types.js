9e2dc2ad9a7f12cc2d6fe682c3bea26474ed14b9
Alex K <smthng@yandex.ru>
2020-07-22 15:13:13 +0300

    pull dev

--- client/src/redux/actions/action-types.js
+++ client/src/redux/actions/action-types.js
@@ -13,8 +13,5 @@ export const ADD_CAFE_EVENTS = 'ADD_CAFE_EVENTS';
 export const LOAD_ALL_EVENTS = 'LOAD_ALL_EVENTS';
 export const LOAD_ALL_EVENTS_SAGA = 'LOAD_ALL_EVENTS_SAGA';
 export const ADD_CAFE = 'ADD_CAFE';
-<<<<<<< HEAD
 export const ADD_RATE = 'ADD_RATE';
-=======
 export const SEARCH_CAFE = 'SEARCH_CAFE';
->>>>>>> a59aa9d42b6ec5b10aa1df6ad2716335928d7031
--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -38,17 +38,14 @@ export function addNewCafe(data) {
   };
 }
 
-<<<<<<< HEAD
 export function addRate(rate) {
   return {
     type: ADD_RATE,
     payload: rate,
   };
-=======
 export function searchCafe(data) {
   return {
     type: SEARCH_CAFE,
     payload: data,
   }
->>>>>>> a59aa9d42b6ec5b10aa1df6ad2716335928d7031
 }
