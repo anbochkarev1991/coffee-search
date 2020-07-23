940fe966261ac551179ca72df1d1c15985b0fa65
Alex K <smthng@yandex.ru>
2020-07-23 16:00:10 +0300

    bar styles

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import './Navbar.module.css';
+import styles from './Navbar.module.css';
 
 function Navbar() {
   const user = useSelector((state) => state.enter.login);
 
   return (
     <>
-      <nav className={"navbar navbar-expand-lg navbar-dark bg-light d-flex justify-content-between"}>
-        <ul className={"nav nav-tabs"}>
-         <NavLink exact to="/" className={"nav-item nav-link"}>На главную</NavLink>
-          {!user && <NavLink to="/login" className={"nav-item nav-link"}>Войти</NavLink>}
-          {user && <NavLink to="/logout" className={"nav-item nav-link"}>Выйти</NavLink>}
-          {user && <NavLink to="/profile" className={"nav-item nav-link"}>Личный профиль</NavLink>}
-          {!user && <NavLink to="/signup" className={"nav-item nav-link"}>Зарегистрироваться</NavLink>}
+      <nav
+        className={
+          'navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between'
+        }
+      >
+        <ul className={'nav nav-tabs'}>
+          <NavLink exact to="/" className={'nav-item nav-link'}>
+            На главную
+          </NavLink>
+          {!user && (
+            <NavLink to="/login" className={'nav-item nav-link'}>
+              Войти
+            </NavLink>
+          )}
+          {user && (
+            <NavLink to="/logout" className={'nav-item nav-link'}>
+              Выйти
+            </NavLink>
+          )}
+          {user && (
+            <NavLink to="/profile" className={'nav-item nav-link'}>
+              Личный профиль
+            </NavLink>
+          )}
+          {!user && (
+            <NavLink to="/signup" className={'nav-item nav-link'}>
+              Зарегистрироваться
+            </NavLink>
+          )}
         </ul>
         <div className="d-flex justify-content-end">
-          {user &&<h5> Добро пожаловать, {user}</h5>}
+          {user && <h5> Добро пожаловать, {user}</h5>}
         </div>
       </nav>
     </>
